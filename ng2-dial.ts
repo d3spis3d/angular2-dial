83cf98790998eb8d79aa8bea556f41f6c2ab6548
Daniel Budden <daniel.l.budden@gmail.com>
2016-03-15 17:41:24 +1100

    Setup export of components

---
+++ ng2-dial.ts
@@ -0,0 +1,8 @@
+import {UniDial} from './src/components/ng2-uni-dial';
+import {BiDial} from './src/components/ng2-bi-dial';
+
+export {UniDial, BiDial};
+
+export default {
+    directives: [UniDial, BiDial]
+}
