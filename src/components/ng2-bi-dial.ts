14025c3d2840437000df664b419f23a8bad4b5a0
Daniel Budden <daniel.l.budden@gmail.com>
2016-03-16 11:25:34 +1100

    Start the bidirectional dial

--- src/components/ng2-bi-dial.ts
+++ src/components/ng2-bi-dial.ts
@@ -2,7 +2,8 @@ import {Component} from 'angular2/core';
 
 @Component({
     selector: 'bi-dial',
-    templateUrl: './ng2-bi-dial.html',
+    templateUrl: 'src/components/ng2-bi-dial.html',
+    styleUrl: ['src/components/ng2-bi-dial.css', 'src/components/ng2-dial-common.css'],
     inputs: ['min', 'max']
 })
 export class BiDial {
